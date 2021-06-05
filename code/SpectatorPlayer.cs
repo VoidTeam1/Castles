0c459972596d1efcf5a11e698f56ce3c1ecf7d9e
MenschlichTv <Menschlich@Humanity-media.at>
2021-06-06 00:35:13 +0200

    feat: proper SpectatorPlayer setup; Added ghost model

--- code/SpectatorPlayer.cs
+++ code/SpectatorPlayer.cs
@@ -4,6 +4,22 @@ namespace Castles
 {
 	public class SpectatorPlayer : Player
 	{
+
+		public override void Respawn()
+		{
+			SetModel( "models/ghost.vmdl" );
+			
+			Controller = new NoclipController();
+			Animator = new StandardPlayerAnimator();
+			Camera = new FirstPersonCamera();
+			
+			EnableDrawing = false;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+			EnableAllCollisions = false;
+
+			base.Respawn();
+		}
 		
 	}
 }
