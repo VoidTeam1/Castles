75d61b6e6a3c3b398879df6e394f346effb609fc
m0uka <lukasekmoucka0@gmail.com>
2021-06-08 14:53:13 +0200

    feat: add PlayerGold.cs

---
+++ code/PlayerGold.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+
+namespace Castles
+{
+	public partial class GamePlayer
+	{
+		[Net]
+		public int Gold { get; set; }
+
+		public bool CanAfford( int gold ) => Gold >= gold;
+
+		public void AddGold( int gold )
+		{
+			Gold += gold;
+		}
+
+		public bool TakeGold( int gold )
+		{
+			if ( !CanAfford( gold ) ) return false;
+			Gold -= gold;
+
+			return true;
+		}
+	}
+}
